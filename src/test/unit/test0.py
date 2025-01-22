22dad57bbee4c626abd11f25e723e2923ece3a9f
Kristian Laudrup Hansen <mlk299@pm.me>
2025-01-22 17:00:46 +0100

    unit tests

--- src/test/unit/test0.py
+++ src/test/unit/test0.py
@@ -1,35 +1,31 @@
 import os
 import sys
 import unittest
-from jax import jacfwd, jacrev
-import jax.numpy as jnp
+from numpy import *
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-import output.add_out as add
+import input.add as add
+import input.ln as ln
 
-def f(x): return jnp.array([x,2]) + jnp.array([2.1,x])
-def g(x): return jnp.array([x,2]) - jnp.array([2.1,x])
+""" class TestPyInputFiles(unittest.TestCase):
 
-class TestAdd(unittest.TestCase):
+    '''
+    Testing:
+    All .py input files from ../test/input/ can compute and run as python scripts
+    '''
 
-    def test_f_fwd_diff(self):
-        fdx = jacfwd(f)
-        result = tuple(r.item() for r in fdx(2.0))
-
+    def test_add(self):
+        
         self.assertEqual(
-            add.f_diff(2.0,1.0),
-            result
+            add.f(2.0),
+            (4.1, 4.0)
         )
 
-    def test_g_fwd_diff(self):
-        fdx = jacfwd(g)
-        result = tuple(r.item() for r in fdx(2.0))
-
         self.assertEqual(
-            add.g_diff(2.0,1.0),
-            result
-        )
+            add.g(2.0),
+            (0.1, 0)
+        ) """
 
 if __name__ == '__main__':
     unittest.main()
